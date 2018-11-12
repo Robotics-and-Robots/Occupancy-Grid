59a55348ae500bb2d5719930a57d4e6d1ac2b80b
andersondomingues <andersondomingues>
2018-11-12 21:37:12 -0200

    fixed printing

--- src/OccupancyGrid.cpp
+++ src/OccupancyGrid.cpp
@@ -46,73 +46,15 @@ OGCellType OccupancyGrid::Set(int x, int y, OGCellType cvalue){
 	if(a >= OG_SEC_W) return 0;
 	if(b >= OG_SEC_H) return 0;
 
-	// //return values for x-positive slice of grid
-	// if(x >= 0) return (y >= 0) ? (_m_pospos[a][b] += cvalue) : (_m_posneg[a][b] += cvalue);
-
-	// //return values for x-negative slice of grid
-	// return (y >= 0) ? (_m_negpos[a][b] += cvalue) : (_m_negneg[a][b] += cvalue);
-
-	//return values for x-positive slice of grid
-
 	double newValue = this->Get(x, y) + cvalue;
 
 	if(newValue < HIMM_THRESHOLD_MIN) return 0;
 	if(newValue > HIMM_THRESHOLD_MAX) return 0;
 	
-	if(x >= 0) 	return (y >= 0) ? (_m_pospos[a][b] = newValue) : (_m_posneg[a][b] = newValue);
-				return (y >= 0) ? (_m_negpos[a][b] = newValue) : (_m_negneg[a][b] = newValue);
-
-
-
-	// if( x >= 0){
-	// 	if( y >= 0){
-	// 		if(cvalue > 0 ){
-	// 			if(_m_pospos[a][b] + cvalue < HIMM_THRESHOLD_MAX){	// increment
-	// 				return _m_pospos[a][b] += cvalue;
-	// 			}	
-	// 		}else {
-	// 			if(_m_pospos[a][b] + cvalue > HIMM_THRESHOLD_MIN){	// decrement
-	// 				return _m_pospos[a][b] += cvalue;
-	// 			}	
-	// 		}
-				
-			
-	// 	}else{ // y < 0
-	// 		if(cvalue > 0 ){
-	// 			if(_m_posneg[a][b] + cvalue < HIMM_THRESHOLD_MAX){	// increment	
-	// 				return _m_posneg[a][b] += cvalue;
-	// 			}	
-	// 		}else {
-	// 			if(_m_posneg[a][b] + cvalue > HIMM_THRESHOLD_MIN){	// decrement
-	// 				return _m_posneg[a][b] += cvalue;
-	// 			}	
-	// 		}
-	// 	}
-	// }else{	// x < 0
-	// 	if( y >= 0){
-	// 		if(cvalue > 0 ){
-	// 			if(_m_negpos[a][b] + cvalue  < HIMM_THRESHOLD_MAX){	// increment
-	// 				return _m_negpos[a][b] += cvalue;
-	// 			}	
-	// 		}else {
-	// 			if(_m_negpos[a][b] + cvalue > HIMM_THRESHOLD_MIN){	// decrement
-	// 				return _m_negpos[a][b] += cvalue;
-	// 			}	
-	// 		}
-				
-			
-	// 	}else{ // y < 0
-	// 		if(cvalue > 0 ){
-	// 			if(_m_negneg[a][b] + cvalue  < HIMM_THRESHOLD_MAX){	// increment
-	// 				return _m_negneg[a][b] += cvalue;
-	// 			}	
-	// 		}else {
-	// 			if(_m_negneg[a][b] + cvalue > HIMM_THRESHOLD_MIN){	// decrement
-	// 				return _m_negneg[a][b] += cvalue;
-	// 			}	
-	// 		}
-	// 	}
-	// }
+	if(x >= 0) 
+		return (y >= 0) ? (_m_pospos[a][b] = newValue) : (_m_posneg[a][b] = newValue);
+	else
+		return (y >= 0) ? (_m_negpos[a][b] = newValue) : (_m_negneg[a][b] = newValue);
 }
 
 /**
@@ -140,37 +82,48 @@ void OccupancyGrid::ToFile(std::string filename){
 	std::stringstream ss;
 
 	ss << "<html>\n"
-       << "<head>\n"
-       << "<meta http-equiv=\"refresh\" content=\"1\" />\n"
-       << "</head>\n"
-	   << "<body style='background:black;'>\n"
-	   << "<div style='text-align:center;margin:auto;'>\n"
-       << "<h3 style='color:white;font-family:sans'>Occupancy Grid</h3><br />\n"
-	   << "<svg style='width:" << (OG_WIDTH*4) << "px; height:" << (OG_HEIGHT*4) << "px; diaply:block; margin:auto'>\n";
+		<< "<head>\n"
+		<< "<meta http-equiv=\"refresh\" content=\"1\" />\n"
+		<< "</head>\n"
+	   	<< "<body style='background:black;'>\n"
+	   	<< "<div style='text-align:center;margin:auto;'>\n"
+		<< "<h3 style='color:white;font-family:sans'>Occupancy Grid</h3><br />\n"
+		<< "<svg style='width:" << OG_WIDTH << "px; height:" << OG_HEIGHT << "px; display:block; margin:auto'>\n";
+
 	int w = 0; 
 	int h = 0;
+
+	//from the maximum Y to the minimum Y, do
 	for(int y = OG_SEC_H; y > -OG_SEC_H; y--){
+
+		//from the minimum X to the maximum X, do
 		for(int x = -OG_SEC_W; x < OG_SEC_W; x++){
 
-			uint32_t vv; 
-			vv = (this->Get(x, y) * (255 / (HIMM_THRESHOLD_MAX - HIMM_THRESHOLD_MIN)));
+			//convert cell values to a grayscale color
+			uint32_t gscolor;
+		        gscolor = this->Get(x, y) * (255 / (HIMM_THRESHOLD_MAX - HIMM_THRESHOLD_MIN));
 
-			if (this->Get(x, y) > 0)
-			    ss << "<circle cx='" << (w*4) << "' cy='" << (h*4) << "' r='1' stroke-width='3' " 
-                   << "stroke='#" << std::hex <<  vv << vv << vv << "' />";
-			else if(x == 0 || y == 0)
-				ss << "<circle cx='" << (w*4) << "' cy='" << (h*4) << "' r='1' stroke-width='1' " 
-                   << "stroke='gray' />";
+			ss << "<circle cx=" << w << " cy=" << h << " r=1 stroke-width=1 ";
+
+			//if the cell is over the x- or y-axis, print it as purple
+			if(x == 0 || y == 0){
+				ss << " stroke='gray' ";
+			}else{
+				ss << " stroke='#" << std::hex << vv << vv << vv << "' ";
+			}
+
+			ss << " /> ";
+
 			w++;
 		}
+
 		h++;
 		w = 0;
-		ss << std::endl;
 	}
 
 	ss << "</div></svg>"
-       << "</body>"
-       << "</html>";
+		<< "</body>"
+		<< "</html>";
 
 	of.open(filename.c_str(), std::ofstream::trunc);
 	of << ss.str();
